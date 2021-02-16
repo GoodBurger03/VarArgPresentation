47190e4a7a4dcd80916a0fbbd8105e257d107fa6
JBartels <busybody0311@gmail.com>
2021-02-16 13:17:16 -0500

    VarArgs presentation

---
+++ src/VarArgsPres.java
@@ -0,0 +1,29 @@
+
+public class VarArgsPres {
+
+	public int noVarArg(int i, int j) {
+
+		return i + j;
+
+	}
+//	
+//	public int VarArg(int ... j) {
+//		int sum =0;
+//		for(int i : j) {
+//			sum = sum + 1;
+//		}
+//		
+//		
+//		return sum;
+//		
+//	}
+
+	public static void main(String[] args) {
+		VarArgsPres object = new VarArgsPres();
+		VarArgsPres object2 = new VarArgsPres();
+
+//		System.out.println(object.noVarArg(4, 8, 12));
+//		 System.out.println(object2.VarArg(4, 8, 12));
+	}
+
+}
